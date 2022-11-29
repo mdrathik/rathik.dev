c4b4b567530a39440630c105dda387237235cd97
Md Rathik <hello@rathik.dev>
2022-11-29 12:32:22 +0600

    update

--- layouts/SnippetsLayout.js
+++ layouts/SnippetsLayout.js
@@ -4,7 +4,7 @@ export default function SnippetsLayout({ title, snippets }) {
   return (
     <>
       <div className="divide-y">
-        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             {title}
           </h1>
@@ -13,7 +13,7 @@ export default function SnippetsLayout({ title, snippets }) {
           </p>
         </div>
         <div className="container py-12">
-          <div className="lg:grid grid-cols-2 gap-6">
+          <div className="grid-cols-2 gap-6 lg:grid">
             {snippets.map((snippet) => (
               <SnippetCard key={snippet.title} snippet={snippet} />
             ))}
