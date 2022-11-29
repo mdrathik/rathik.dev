e26d87fdc963556b790a7dad9d4f3f7d87d0efc0
Md Rathik <hello@rathik.dev>
2022-11-30 00:57:59 +0600

    update comment

--- next.config.js
+++ next.config.js
@@ -5,12 +5,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' utteranc.es;
-  style-src 'self' 'unsafe-inline';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' utteranc.es https://www.googletagmanager.com 'unsafe-inline';
+  object-src 'self';
+  style-src 'self' 'unsafe-inline' *.googleapis.com cdn.jsdelivr.net  ;
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
-  font-src 'self';
+  font-src 'self' fonts.gstatic.com cdn.jsdelivr.net;
   frame-src utteranc.es
 `
 
