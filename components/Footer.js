bbbabcf930b78c9ab83b8f6c001593b1ffb53bd2
Md Rathik <hello@rathik.dev>
2022-12-01 17:56:38 +0600

    update

--- components/Footer.js
+++ components/Footer.js
@@ -17,9 +17,7 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
-          </Link>
+          This nextjs tailwind-blog built by @Timothy
         </div>
       </div>
     </footer>
