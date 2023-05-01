b107b4a83a797728418f8e4180639abb27f1573c
Md Rathik <hello@rathik.dev>
2023-05-01 23:48:53 +0600

    📝 docs(laravel): add tutorial on Laravel's optional() helper function 🎨 style(headerNavLinks.js): comment out unused links 🖼️ feat(laravel): add image for Laravel's optional() helper function tutorial

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,9 +1,10 @@
 export let headerNavLinks = [
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
+  // { href: '/tags', title: 'Tags' },
   { href: '/snippets', title: 'Snippets' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  // { href: '/contact', title: 'Contact' },
 ]
 
 export default headerNavLinks
