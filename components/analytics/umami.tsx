31fb1ab8a9470cc812c96f56ec287a5d38b9f639
Md Rathik <rathik@bzmgraphics.com>
2024-12-06 19:42:30 +0600

    update unamianalytics

--- components/analytics/umami.tsx
+++ components/analytics/umami.tsx
@@ -1,13 +1,9 @@
-import Script from 'next/script.js'
+import Script from 'next/script'
 
-interface UmamiAnalyticsProps {
-  websiteId?: string
-  src?: string
-}
+export function UmamiAnalytics() {
+  // Hardcoded values
+  const websiteId = 'e390f4d5-d20c-474a-87a7-4318fa7ba907'
+  const scriptSrc = 'https://cloud.umami.is/script.js' // Direct URL to the Umami Cloud script
 
-export function UmamiAnalytics({ websiteId, src = '/stats/script.js' }: UmamiAnalyticsProps) {
-  if (websiteId) {
-    return <Script async defer data-website-id={websiteId} src={src} />
-  }
-  return null
+  return <Script async defer data-website-id={websiteId} src={scriptSrc} />
 }
